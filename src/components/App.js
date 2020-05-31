daf9e19c99f1fbe4a6981e49ffee2bf4d1cab418
Sone <sthaya@gmail.com>
2020-05-31 13:39:50 -0700

    created taskform component to handle input

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import TaskList from './TaskList'
 import TaskListContextProvider from '../context/TaskListContext'
+import TaskForm from './TaskForm'
 import '../App.css'
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
       <div className='container'>
         <div className="app-wrapper">
           <div className="main">
+            <TaskForm />
             <TaskList />
           </div>
         </div>
